8360fb9854436677b9cb28125d1415c56dfe040e
muhamadd9 <mhmdramadan.192@gmail.com>
2024-04-28 15:45:35 +0300

    added new problem

---
+++ 7kyu/regex-validate-PIN-code.js
@@ -0,0 +1,23 @@
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+
+// Examples (Input --> Output)
+// "1234"   -->  true
+// "12345"  -->  false
+// "a234"   -->  false
+
+// Solution 
+
+function validatePIN(pin) {
+    let pinNumber = Number(pin);
+    let bool = true ;
+    if(isNaN(pinNumber) || !/^\d+$/.test(pin) || pin.length != 4 && pin.length != 6 ) bool = false;
+    if(bool){
+        return true ;
+    }
+    else{
+        return false ;
+    }
+    
+}
