71be9ffcd619fee5c16b1f7e767bff6870b72c71
muhamadd9 <mhmdramadan.192@gmail.com>
2024-05-01 20:35:12 +0300

    added new problem

---
+++ 8kyu/rock-Paper-Scissors.js
@@ -0,0 +1,22 @@
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples(Input1, Input2 --> Output):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+const rps = (p1, p2) => {
+    let x ;
+    if(p1 == p2){
+      x = "Draw!"
+    }
+    else if((p1 == 'scissors' && p2=='paper') ||( p1 == 'rock' && p2=='scissors') || (p1 == 'paper' && p2=='rock')){
+      x = "Player 1 won!"
+    }
+    else{
+      x = "Player 2 won!"
+    }
+    return x
+  
+  };
