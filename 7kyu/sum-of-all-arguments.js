60fe0149e7590f628d4c989e1b4ad6a00752e8fc
muhamadd9 <mhmdramadan.192@gmail.com>
2024-05-09 14:02:50 +0300

    added new problem and note

---
+++ 7kyu/sum-of-all-arguments.js
@@ -0,0 +1,17 @@
+// Write a function that finds the sum of all its arguments.
+
+// eg:
+
+// sum(1, 2, 3) // => 6
+// sum(8, 2) // => 10
+// sum(1, 2, 3, 4, 5) // => 15
+
+// My Solution
+
+function sum() {
+    let sum = 0 ;
+    for(let i = 0 ; i<arguments.length;i++){
+        sum += arguments[i];
+    }
+    return sum
+}
