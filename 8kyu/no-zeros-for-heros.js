298a1041457d93335ed1fe4ea66d44086f1d9a39
muhamadd9 <mhmdramadan.192@gmail.com>
2024-05-13 14:42:58 +0300

    Added New Problem

---
+++ 8kyu/no-zeros-for-heros.js
@@ -0,0 +1,26 @@
+// Numbers ending with zeros are boring.
+
+// They might be fun in your world, but not here.
+
+// Get rid of them. Only the ending ones.
+
+// 1450 -> 145
+// 960000 -> 96
+// 1050 -> 105
+// -1050 -> -105
+
+// My Solution 
+
+function noBoringZeros(n) {
+    let numArr =  n.toString().split("")
+    let newArr = []
+    let foundNonZero = false ;
+    for(let i = numArr.length-1 ; i >=0 ; i--){
+      if (numArr[i] !== "0" || foundNonZero){
+        newArr.unshift(numArr[i]) 
+        foundNonZero = true
+      }
+      
+    }
+    return Number(newArr.join(""))
+  }
