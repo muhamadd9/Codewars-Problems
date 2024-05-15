d28f0f2f341d684a71a94c8d2237c2134bb0e8e0
muhamadd9 <mhmdramadan.192@gmail.com>
2024-05-16 02:17:42 +0300

    added new problem

---
+++ 7kyu/strong-Number-Special-Numbers-Series-2.js
@@ -0,0 +1,28 @@
+// Strong number is a number with the sum of the factorial of its digits is equal to the number itself.
+
+// For example, 145 is strong, because 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+// Task
+// Given a positive number, find if it is strong or not, and return either "STRONG!!!!" or "Not Strong !!".
+
+// Examples
+// 1 is a strong number, because 1! = 1, so return "STRONG!!!!".
+// 123 is not a strong number, because 1! + 2! + 3! = 9 is not equal to 123, so return "Not Strong !!".
+// 145 is a strong number, because 1! + 4! + 5! = 1 + 24 + 120 = 145, so return "STRONG!!!!".
+// 150 is not a strong number, because 1! + 5! + 0! = 122 is not equal to 150, so return "Not Strong !!".
+
+
+// My Solution 
+
+function strong(n) {
+    return n.toString().split("").map((num)=>
+    factorial(Number(num))
+    ).reduce((acc,next)=>acc+next,0) === n ? "STRONG!!!!" : "Not Strong !!"
+  }
+  
+  function factorial(n){
+    if(n===0 || n===1 ){
+      return 1
+    }
+    return n* factorial(n-1)
+}
