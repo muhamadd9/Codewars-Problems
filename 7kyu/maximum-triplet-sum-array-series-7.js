8c860ca1e1984eba2d7dbf06e79479a7f42d95ee
muhamadd9 <mhmdramadan.192@gmail.com>
2024-05-19 00:48:29 +0300

    added new problem

---
+++ 7kyu/maximum-triplet-sum-array-series-7.js
@@ -0,0 +1,27 @@
+// Given an array/list [] of n integers , find maximum triplet sum in the array Without duplications .
+
+// Notes :
+// Array/list size is at least 3 .
+
+// Array/list numbers could be a mixture of positives , negatives and zeros .
+
+// Repetition of numbers in the array/list could occur , So (duplications are not included when summing).
+
+// Input >> Output Examples
+// 1- maxTriSum ({3,2,6,8,2,3}) ==> return (17)
+// Explanation:
+// As the triplet that maximize the sum {6,8,3} in order , their sum is (17)
+
+// Note : duplications are not included when summing , (i.e) the numbers added only once .
+
+
+// My Solution 
+
+function maxTriSum(numbers){
+    let sum = 0
+    let sortedNonDuplicatArr= numbers.sort((a,b)=>a-b).filter((value, index, self) => self.indexOf(value) === index)
+    for(let i = 0 ; i<3;i++){
+      sum+= sortedNonDuplicatArr.pop()
+    }
+    return sum
+}
