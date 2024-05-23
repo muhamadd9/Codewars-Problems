8a5732e03b073111cf36aae71abc299080e33e75
muhamadd9 <mhmdramadan.192@gmail.com>
2024-05-24 00:58:07 +0300

    added new problem

---
+++ 7kyu/Is-it-a-letter.js
@@ -0,0 +1,7 @@
+// Write a function, isItLetter or is_it_letter or IsItLetter, which tells us if a given character is a letter or not.
+
+// My Solution 
+
+function isItLetter(character) {
+    return (character.charCodeAt() >= 97 && character.charCodeAt() < 123) || (character.charCodeAt() >= 65 && character.charCodeAt() < 91) ? true:false
+}
