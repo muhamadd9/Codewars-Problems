039b5680c250dfcea757c322e7762023fd04cae5
muhamadd9 <mhmdramadan.192@gmail.com>
2024-05-25 10:54:22 +0300

    added new problem

---
+++ 7kyu/area-of-a-Circle.js
@@ -0,0 +1,18 @@
+// Complete the function which will return the area of a circle with the given radius.
+
+// Returned value is expected to be accurate up to tolerance of 0.01.
+
+// If the radius is not positive, throw Error.
+
+// Example:
+
+// circleArea(43.2673);     // returns 5881.248  (± 0.01)
+// circleArea(68);          // returns 14526.724 (± 0.01)
+// circleArea(0);           // throws Error
+// circleArea(-1);          // throws Error
+
+// My Solution
+
+function circleArea(radius) {
+    return radius>0? radius * radius *Math.PI : throwError("not Possible")
+  }
