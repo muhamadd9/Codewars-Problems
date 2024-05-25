8b6388c96c2bd7e492cad52773f8912f95dbde90
muhamadd9 <mhmdramadan.192@gmail.com>
2024-05-25 10:46:27 +0300

    added new problem

---
+++ 7kyu/how-many-arguments.js
@@ -0,0 +1,11 @@
+// Write a function that returns the number of arguments it received.
+
+// args_count() --> 0
+// args_count('a') --> 1
+// args_count('a', 'b') --> 2
+
+// My Solution 
+
+function args_count(){
+    return arguments.length
+  }
