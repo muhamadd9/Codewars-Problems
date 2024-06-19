c72e50f790e63baace2258a03e6b66414abb0c6b
muhamadd9 <mhmdramadan.192@gmail.com>
2024-06-19 21:13:11 +0300

    added new problem

--- 7kyu/Find-min-and-max.js
+++ 7kyu/Find-min-and-max.js
@@ -16,3 +16,10 @@ function getMinMax(arr){
     }
     return [min,max]
   };
+
+//   Another Solution 
+
+function getMinMax(arr){
+    arr.sort((a,b)=>a-b)
+    return [arr[0],arr[arr.length-1]]
+  };
