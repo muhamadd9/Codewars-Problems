f308af4ac0f7cbc92cef8c3e2675fb924a2314f8
Muhamad Ramadan <149680627+muhamadd9@users.noreply.github.com>
2024-11-04 18:41:59 +0200

    Bit Counting

--- 6kyu/Bit Counting.js
+++ 6kyu/Bit Counting.js
@@ -5,3 +5,11 @@ var countBits = function (n) {
     .map(num => num * 1 === 1 && count++);
   return count;
 };
+
+
+var countBits = function (n) {
+  return n
+    .toString(2)
+    .split('')
+    .filter(num => num == 1).length;
+};
